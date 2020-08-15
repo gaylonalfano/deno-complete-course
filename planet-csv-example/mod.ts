8b5818638a0f08f9e60c126a59e46b7a7e27955f
gaylonalfano <gaylon.alfano@gmail.com>
2020-08-15 15:01:05 +0800

    Reviewed some basic reading/writing files functionality and introduced to lodash helper functions like _.pick()

--- planet-csv-example/mod.ts
+++ planet-csv-example/mod.ts
@@ -1,10 +1,9 @@
-import { join } from 'https://deno.land/std/path/mod.ts';
-import { BufReader } from 'https://deno.land/std/io/bufio.ts';
-import { parse } from 'https://deno.land/std/encoding/csv.ts';
-
+import { join } from "https://deno.land/std/path/mod.ts";
+import { BufReader } from "https://deno.land/std/io/bufio.ts";
+import { parse } from "https://deno.land/std/encoding/csv.ts";
 /* import * as _ from "https://raw.githubusercontent.com/lodash/lodash/4.17.15-es/lodash.js"; */
 /* import * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js"; */
-import { pick } from 'https://deno.land/x/lodash@4.17.15-es/lodash.js';
+import { pick } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
 
 // Define TS interface for planet
 // Going to use this interface in a Type Assertion for 'result' Array
@@ -15,20 +14,22 @@ interface Planet {
 }
 
 async function loadPlanetsData() {
-  const path = join('.', 'kepler_exoplanets_nasa.csv');
+  const path = join(".", "kepler_exoplanets_nasa.csv");
   const file = await Deno.open(path);
   const bufReader = new BufReader(file);
-  const result = await parse(bufReader, { header: true, comment: '#' });
+  const result = await parse(bufReader, { header: true, comment: "#" });
   Deno.close(file.rid);
+
+  /* console.log(result); */
 
   const planets = (result as Array<Planet>).filter((planet) => {
     // Convert to Number() since CSV is all text
-    const planetaryRadius = Number(planet['koi_prad']);
-    const stellarMass = Number(planet['koi_smass']);
-    const stellarRadius = Number(planet['koi_srad']);
+    const planetaryRadius = Number(planet["koi_prad"]);
+    const stellarMass = Number(planet["koi_smass"]);
+    const stellarRadius = Number(planet["koi_srad"]);
 
     return (
-      planet['koi_disposition'] === 'CONFIRMED' &&
+      planet["koi_disposition"] === "CONFIRMED" &&
       planetaryRadius > 0.5 &&
       planetaryRadius < 1.5 &&
       stellarMass > 0.78 &&
@@ -42,46 +43,60 @@ async function loadPlanetsData() {
   /* return planets; */
   // Use the lodash _.pick() helper function
   return planets.map((planet) => {
-    return pick(planet, [
-      'koi_prad',
-      'koi_smass',
-      'koi_srad',
-      'koi_time0bk',
-      'koi_period',
-      'kepler_name',
-      'koi_count',
-      'koi_steff',
-    ]);
+    console.log(
+      pick(planet, [
+        "koi_prad",
+        "koi_smass",
+        "koi_srad",
+        "koi_time0bk",
+        "koi_period",
+        "kepler_name",
+        "koi_count",
+        "koi_steff",
+      ])
+    );
+    /* return pick(planet, [ */
+    /*   "koi_prad", */
+    /*   "koi_smass", */
+    /*   "koi_srad", */
+    /*   "koi_time0bk", */
+    /*   "koi_period", */
+    /*   "kepler_name", */
+    /*   "koi_count", */
+    /*   "koi_steff", */
+    /* ]); */
   });
 }
 
-const newEarths = await loadPlanetsData();
-for (const planet of newEarths) {
-  console.log(planet);
-  // Below: without lodash.pick() function have to manually select keys
-  /* console.log( */
-  /*   planet["kepler_name"], */
-  /*   planet["kepoi_name"], */
-  /*   planet["koi_prad"], */
-  /*   planet["koi_smass"], */
-  /*   planet["koi_srad"] */
-  /* ); */
-}
-console.log(`${newEarths.length} habitable planets found!`);
+await loadPlanetsData();
 
-const orbitTimes: Array<number> = [];
-for (const planet of newEarths) {
-  orbitTimes.push(planet['koi_period']);
-}
-const minOrbit: number = Math.min(...orbitTimes);
-
-console.log(orbitTimes);
-console.log(minOrbit);
-// Basic example of reading a text file
-/* async function readFile() { */
-/*   const path = join("text-files", "hello.txt"); */
-/*   const data = await Deno.readTextFile(path); */
-/*   console.log(data); */
+/* const newEarths = await loadPlanetsData(); */
+/* for (const planet of newEarths) { */
+/*   console.log(planet); */
+/*   // Below: without lodash.pick() function have to manually select keys */
+/*   /1* console.log( *1/ */
+/*   /1*   planet["kepler_name"], *1/ */
+/*   /1*   planet["kepoi_name"], *1/ */
+/*   /1*   planet["koi_prad"], *1/ */
+/*   /1*   planet["koi_smass"], *1/ */
+/*   /1*   planet["koi_srad"] *1/ */
+/*   /1* ); *1/ */
 /* } */
+/* console.log(`${newEarths.length} habitable planets found!`); */
 
-/* await readFile(); */
+/* const orbitTimes: Array<number> = []; */
+/* for (const planet of newEarths) { */
+/*   orbitTimes.push(planet['koi_period']); */
+/* } */
+/* const minOrbit: number = Math.min(...orbitTimes); */
+
+/* console.log(orbitTimes); */
+/* console.log(minOrbit); */
+/* // Basic example of reading a text file */
+/* /1* async function readFile() { *1/ */
+/* /1*   const path = join("text-files", "hello.txt"); *1/ */
+/* /1*   const data = await Deno.readTextFile(path); *1/ */
+/* /1*   console.log(data); *1/ */
+/* /1* } *1/ */
+
+/* /1* await readFile(); *1/ */
