59bdd6806de7afef958c084dc950ebe1b605f527
gaylonalfano <gaylon.alfano@gmail.com>
2020-10-25 20:00:43 +0800

    BREAKING CHANGES. Completely removed the monorepo structure to account for Drash server. Simplified and flattened the project structure. Also renamed app.ts to drash.server.ts and mod.ts to oak.server.ts for clarity. Keeping both for comparisons.

--- student-enrollment-drash/drash.server.ts
+++ student-enrollment-drash/drash.server.ts
@@ -5,32 +5,32 @@ import StudentResource from "./resources/student.resource.ts";
 import FileResource from "./resources/file.resource.ts";
 
 // ===== SINGLE DIRECTORY (/backend/public) **WORKS**
-// const server = new Drash.Http.Server({
-//   response_output: "text/html",
-//   resources: [HomeResource, StudentResource, FileResource],
-//   // directory: "../frontend", // "/path/to/your/project"
-//   // static_paths: ["/src"], // ["/public"] - **Routes this path to any client i.e., makes it publicly available
-//   // views_path: "./src/views", // ["./public/views"]
-//   // NOTE: It may require /path/to/your/DRASH/project (ie /backend)
-//   directory: ".",
-//   static_paths: ["/public"],
-//   views_path: "./public/views",
-// });
-
-// ===== MONOREPO (/backend + /frontend/public) **BROKEN**
 const server = new Drash.Http.Server({
   response_output: "text/html",
   resources: [HomeResource, StudentResource, FileResource],
-  // directory: "../frontend", // "/path/to/your/DRASH/project"
+  // directory: "../frontend", // "/path/to/your/project"
   // static_paths: ["/src"], // ["/public"] - **Routes this path to any client i.e., makes it publicly available
   // views_path: "./src/views", // ["./public/views"]
   // NOTE: It may require /path/to/your/DRASH/project (ie /backend)
   directory: ".",
-  static_paths: ["../frontend/public"],
-  views_path: "../frontend/public/views",
+  static_paths: ["/public"],
+  views_path: "./public/views",
 });
 await server.run({
   hostname: "localhost",
   port: 1447,
 });
 console.log(`Server running on http://${server.hostname}:${server.port}`);
+
+// ===== MONOREPO (/backend + /frontend/public) **BROKEN**
+// const server = new Drash.Http.Server({
+//   response_output: "text/html",
+//   resources: [HomeResource, StudentResource, FileResource],
+//   // directory: "../frontend", // "/path/to/your/DRASH/project"
+//   // static_paths: ["/src"], // ["/public"] - **Routes this path to any client i.e., makes it publicly available
+//   // views_path: "./src/views", // ["./public/views"]
+//   // NOTE: It may require /path/to/your/DRASH/project (ie /backend)
+//   directory: ".",
+//   static_paths: ["../frontend/public"],
+//   views_path: "../frontend/public/views",
+// });
