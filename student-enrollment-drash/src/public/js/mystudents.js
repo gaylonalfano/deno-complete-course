8be3fb1992bdcef2f38d44438c19ee726ca4de8d
gaylonalfano <gaylon.alfano@gmail.com>
2020-10-29 10:24:33 +0800

    Successfully searching for all studentss by pointing to API endpoint and then setting the <section> innerHTML to be the response.text() output. Very basic but it remains on the home page rather than changing URLs. Next, need to add logic for individual student ID search and or grade search.

--- student-enrollment-drash/src/public/js/mystudents.js
+++ student-enrollment-drash/src/public/js/mystudents.js
@@ -1,21 +1,33 @@
 // @ts-nocheck
 document.addEventListener("DOMContentLoaded", function () {
   // Retrieve the values entered into Search Form fields (div class="form-control")
+  const formElement = document.getElementById("search-form");
+  const resultElement = document.getElementById("search-result");
   const studentIdElement = document.getElementById("student-id");
   const gradeElement = document.getElementById("grade");
   const buttonElement = document.getElementById("search-button");
 
+  // Search form example: https://www.youtube.com/watch?v=wjgiPFWvwBI
+  const apiURL = "http://localhost:1447";
+
   async function submitHandler(event) {
     event.preventDefault();
-    console.log("Button clicked!!!");
+    // console.log("Button clicked!!!");
+    const studentIdSearchValue = studentIdElement.value;
+    const gradeSearchValue = gradeElement.value;
 
     // Perform check on whether they were entered
     // FIXME What if enter both search fields?
-    if (studentIdElement.value) {
-      console.log(`Entered Student Id: ${studentIdElement.value}`);
+    if (!studentIdSearchValue && !gradeSearchValue) {
+      console.log("No search criteria entered. Returning all students.");
+      const searchResult = await fetch(`${apiURL}/students`);
+      const data = await searchResult.text(); // WORKS
+      // const data = await searchResult.json(); // [object Object][object Object]
+      console.log(data);
+      resultElement.innerHTML = data;
 
       // Just redirect to the right path/URL?
-      location.href = `/students/id/${studentIdElement.value}`;
+      // location.href = `/students/id/${studentIdSearchValue}`;
 
       // TODO ?? Do I need to handle the Promise or does Drash do that?
       // return await fetch(`/students/id/${studentIdElement.value}`, {
@@ -27,15 +39,16 @@ document.addEventListener("DOMContentLoaded", function () {
       //   .then((response) => response.text())
       //   // .then((response) => response.json());
       //   .then((data) => console.log(data));
-    } else {
-      console.log("Clicked without StudentID!");
-      // return fetch("/students", {
-      //   method: "GET",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      // });
     }
+    // else {
+    //   console.log("Clicked without StudentID!");
+    //   // return fetch("/students", {
+    //   //   method: "GET",
+    //   //   headers: {
+    //   //     "Content-Type": "application/json",
+    //   //   },
+    //   // });
+    // }
   }
   buttonElement.addEventListener("click", submitHandler);
 });
